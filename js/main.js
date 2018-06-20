11e3f068395ebd9452c9750a0ed02f4297ef4ae2
Itzik Turjeman <itzikit@gmail.com>
2018-06-20 17:16:25 +0300

    onClick functions and CSS updates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+function onImgClick(imgId) {
+    var elGalery = document.querySelector(".galery");
+    elGalery.classList.add(".hidden");
+
+    var elCanvas = document.querySelector("#canvas");
+    elCanvas.classList.remove(".hidden");
+    imgClicked(imgId, elCanvas);
+}
