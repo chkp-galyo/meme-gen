ceaba0e0da2f89108e226cefa109d92107f738f7
galyo9 <galyogev5@gmail.com>
2018-06-20 15:47:39 +0300

    gImgs created and imgs changed name

--- js/meme-service.js
+++ js/meme-service.js
@@ -1,15 +1,37 @@
 'use strict';
 
 
-var gImgs = [];
 
-var img = {
-    id: 1111,
-    url: eiuqwyeiuqwyeiuqw,
-    keywords: [],
-}
 
-var gImgs = [{ id: 1, url: 'img/img6.jpg', keywords: ['happy'] }];
+
+var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['happy', 'movies'] },
+{ id: 2, url: 'img/2.jpg', keywords: ['happy', 'dance', 'movies'] },
+{ id: 3, url: 'img/3.jpg', keywords: ['angry'] },
+{ id: 4, url: 'img/4.jpg', keywords: ['love'] },
+{ id: 5, url: 'img/5.jpg', keywords: ['happy', 'win', 'baby'] },
+{ id: 6, url: 'img/6.jpg', keywords: ['sleep', 'baby', 'dog'] },
+{ id: 7, url: 'img/7.jpg', keywords: ['sleep', 'cat'] },
+{ id: 8, url: 'img/8.jpg', keywords: ['funny', 'movies'] },
+{ id: 9, url: 'img/9.jpg', keywords: ['funny', 'baby'] },
+{ id: 10, url: 'img/10.jpg', keywords: ['funny'] },
+{ id: 11, url: 'img/11.jpg', keywords: ['happy'] },
+{ id: 12, url: 'img/12.jpg', keywords: ['funny'] },
+{ id: 13, url: 'img/13.jpg', keywords: ['funny', 'movies'] },
+{ id: 14, url: 'img/14.jpg', keywords: ['happy', 'dance'] },
+{ id: 15, url: 'img/15.jpg', keywords: ['angry'] },
+{ id: 16, url: 'img/16.jpg', keywords: ['funny', 'baby'] },
+{ id: 17, url: 'img/17.jpg', keywords: ['funny', 'dog'] },
+{ id: 18, url: 'img/18.jpg', keywords: ['love', 'sport'] },
+{ id: 19, url: 'img/19.jpg', keywords: ['angry'] },
+{ id: 20, url: 'img/20.jpg', keywords: ['serious', 'movies'] },
+{ id: 21, url: 'img/21.jpg', keywords: ['happy', 'movies'] },
+{ id: 22, url: 'img/22.jpg', keywords: ['happy'] },
+{ id: 23, url: 'img/23.jpg', keywords: ['funny', 'movies'] },
+{ id: 24, url: 'img/24.jpg', keywords: ['putin', 'funny'] },
+{ id: 25, url: 'img/25.jpg', keywords: ['funny', 'movies'] },
+];
+
+
 var gMeme = {
     selectedImgId: 5,
     txts: [
