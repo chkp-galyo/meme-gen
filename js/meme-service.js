2bcee8dfdc97c7bcd87ddb3542a397bc2844966b
Itzik Turjeman <itzikit@gmail.com>
2018-06-20 15:06:25 +0300

    starting project - main structure

---
+++ js/meme-service.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+var gImgs = [];
+
+var img = {
+    id: 1111,
+    url: eiuqwyeiuqwyeiuqw,
+    keywords: [],
+}
+
+var gImgs = [{ id: 1, url: 'img/img6.jpg', keywords: ['happy'] }];
+var gMeme = {
+    selectedImgId: 5,
+    txts: [
+        {
+            line: 'I never eat Falafel',
+            size: 20,
+            align: 'left',
+            color: 'red'
+        }
+    ]
+}
