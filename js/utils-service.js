875ed246fc38273b61cfcc346bc6453fad8f9199
Itzik Turjeman <itzikit@gmail.com>
2018-06-24 11:19:05 +0300

    css and responsive changes

--- js/utils-service.js
+++ js/utils-service.js
@@ -9,3 +9,10 @@ function loadFromStorage(key) {
     return JSON.parse(localStorage.getItem(key))
 }
 
+function openMenue(x) {
+    x.classList.toggle("change");
+    var elMenu = document.querySelector(".menu");
+    elMenu.classList.toggle("show-menu");
+    elMenu.classList.toggle("open");
+
+}
